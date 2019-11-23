3b5010ed77de0c4ed751d9707806ca47bd2dd027
sasconsul sas1@uchicago.edu <sas1@uchicago.edu>
2019-11-22 20:49:52 -0800

    Header setup

--- src/App.js
+++ src/App.js
@@ -6,18 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Flag Picker
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          This app will help you to learn flags around the world in <u>3 steps</u>.
+        </p>
+
+
       </header>
     </div>
   );
